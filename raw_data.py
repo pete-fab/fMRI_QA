0b73e2b7bf6158391f0f376fc8786da40d238fff
Piotr Faba <piotr.faba@uj.edu.pl>
2016-12-16 20:32:36 +0100

    bug fix in raw_data.py

--- raw_data.py
+++ raw_data.py
@@ -7,17 +7,18 @@ import directory as d
 import datetime as dt
 import dicom
 
+
 la = l.AllLogger()
 
 
 class RawData(object):
     def __init__(self, local_path, server_path, summary_file_path, attribute_list):
         if not d.isDir(local_path):
-            al.exception("Local_path is not existing directory: " + local_path)
+            la.exception("Local_path is not existing directory: " + local_path)
             raise OSError(local_path)
 
-        if not d.isDir(local_path):
-            al.exception("server_path is not existing directory: " + server_path)
+        if not d.isDir(server_path):
+            la.exception("server_path is not existing directory: " + server_path)
             raise OSError(server_path)
 
         self.__is_summary_valid = False
@@ -25,7 +26,8 @@ class RawData(object):
 
         self.__l_path = local_path
         self.__s_path = server_path
-        self.__datetime = dt.datetime(1, 1, 1, 0, 0, 0)
+        self.__analysed_dates = set()
+        # self.__datetime = dt.datetime(1, 1, 1, 0, 0, 0)
         self.__summary_path_unvalidated = summary_file_path
         self.__summary_path = ""
         self.__validate_summary_path(summary_file_path)
@@ -36,7 +38,7 @@ class RawData(object):
         self.update_datasets()
 
     def update_datasets(self):
-        # self.__retrieve_local_sets()
+        self.__retrieve_local_sets()
         self.__retrieve_server_sets()
 
     def set_local_path(self, local_path):
@@ -47,31 +49,28 @@ class RawData(object):
 
     def __retrieve_local_sets(self):
 
-        # Gets date from archives and checks their validity
-        self.__get_date_from_local_archives()
         if not self.__is_summary_valid and self.__is_archive_files_valid:
             # Remake the global summary
-            self.__fix_summary_from_local_acrhives()
+            self.__fix_summary_from_local_archives()
 
+        self.__get_dates_from_local_archives()
         if self.__is_summary_valid:
-            self.__get_data_from_summary()
-            self.__get_date_from_data()
-            # rlogs only useful to narrow down the date within the porgram run cycle
-            self.__get_date_from_rlogs()
+            self.__get_dates_from_summary()
 
     def __retrieve_server_sets(self):
-        self.__datetime = dt.datetime(2016, 9, 10, 0, 0, 0)
         self.__copy_valid_series_from_server()
 
     def __copy_valid_series_from_server(self):
         dir_names = d.getChildrenFolders(self.__s_path)
         dir_names = sorted(dir_names, reverse=True)
         for dn in dir_names:
-            datetime = dt.datetime(int(dn[0:4]), int(dn[4:6]), int(dn[6:]), 0, 0, 0)
-            if datetime > self.__datetime:
-                sub_paths = d.getChildrenPaths(d.joinPath([self.__s_path, dn]))
+            date_time = dt.datetime(int(dn[0:4]), int(dn[4:6]), int(dn[6:]), 0, 0, 0)
+            # print date_time
+            if not self.__analysed_dates.issuperset(set([date_time])):
+            # if datetime > self.__datetime:
+                sub_paths = d.getChildrenPaths(d.joinPath([self.__s_path, date_time]))
                 for sp in sub_paths:
-                    if (self.__verify_is_contains_QA(sp)):
+                    if self.__verify_is_contains_QA(sp):
                         dest = d.joinPath([self.__l_path, d.getNameFromPath(sp, -2)])
                         d.copy_folder_contents(sp, dest)
             else:
@@ -113,47 +112,47 @@ class RawData(object):
         if self.__summary_path != "":
             self.__is_summary_valid = True
 
-    def __get_data_from_summary(self):
-        raise NotImplementedError("Convienient way of combining data later is required")
-
-    def __get_date_from_data(self):
-
+    def __get_dates_from_summary(self):
         if self.__summary_path != "":
             data = qa_csv.read_csv(self.__summary_path)
             if len(data) != 0:
                 date = sorted(data['seriesIndex'], reverse=True)[0]
-                datetime = dt.datetime(int(date[0:4]), int(date[4:6]), int(date[6:]), 0, 0, 0)
-                if datetime > self.__datetime:
-                    self.__datetime = datetime
+                date_time = dt.datetime(int(date[0:4]), int(date[4:6]), int(date[6:]), 0, 0, 0)
+                self.__analysed_dates.add(date_time)
+                # if date_time > self.__datetime:
+                #     self.__datetime = date_time
             else:
                 message = "The global summary file should contain data if exists"
                 la.error(message)
                 raise BufferError(message)
+    #
+    #
+    # #Get latest date from all rlogs
+    # def __get_date_from_rlogs(self):
+    #     rlogs = d.getFilePaths(self.__l_path, ".rlog")
+    #     if len(rlogs) == 0:
+    #         la.warn("No runtime logs found so far. It's OK if this is first run")
+    #         return False
+    #     for r_log in rlogs:
+    #         if hashfile.verify(r_log):
+    #             name = d.getFileName(r_log)
+    #             datetime = dt.datetime(int(name[1:5]), int(name[6:8]), int(name[9:11]), int(name[12:14]),
+    #                                    int(name[15:17]), int(name[18:20]))
+    #             if datetime > self.__datetime:
+    #                 self.__datetime = datetime
+    #         else:
+    #             la.warning("There exists rlog with modified hashfile: " + r_log)
 
-    def __get_date_from_rlogs(self):
-        rlogs = d.getFilePaths(self.__l_path, ".rlog")
-        if len(rlogs) == 0:
-            la.warn("No runtime logs found so far. It's OK if this is first run")
-            return False
-        for r_log in rlogs:
-            if hashfile.verify(r_log):
-                name = d.getFileName(r_log)
-                datetime = dt.datetime(int(name[1:5]), int(name[6:8]), int(name[9:11]), int(name[12:14]),
-                                       int(name[15:17]), int(name[18:20]))
-                if datetime > self.__datetime:
-                    self.__datetime = datetime
-            else:
-                la.warning("There exists rlog with modified hashfile: " + r_log)
-
-    def __get_date_from_local_archives(self):
+    def __get_dates_from_local_archives(self):
         file_paths = d.getFilePaths(self.__l_path, ".gz")
         file_paths = sorted(file_paths)
         for f in file_paths:
             if hashfile.verify(f):
                 name = d.getFileName(f)
-                datetime = dt.datetime(int(name[0:4]), int(name[4:6]), int(name[6:8]), 0, 0, 0)
-                if datetime > self.__datetime:
-                    self.__datetime = datetime
+                date_time = dt.datetime(int(name[0:4]), int(name[4:6]), int(name[6:8]), 0, 0, 0)
+                self.__analysed_dates.add(date_time)
+                # if date_time > self.__datetime:
+                #     self.__datetime = date_time
             else:
                 self.__is_archive_files_valid = False
                 la.warning("There exists an analysis archive with modified hashfile: " + f + ". All will be removed")
@@ -163,7 +162,7 @@ class RawData(object):
                 d.delete(f)
                 la.warning("Removed: " + f)
 
-    def __fix_summary_from_local_acrhives(self):
+    def __fix_summary_from_local_archives(self):
         if self.__is_archive_files_valid:
             file_paths = d.getFilePaths(self.__l_path, ".gz")
             file_paths = sorted(file_paths)
@@ -182,8 +181,8 @@ class RawData(object):
 
             # zip the folders
             dir_paths = d.getChildrenPaths(self.__l_path)
-            for d in dir_paths:
-                d.compress(d)
+            for dp in dir_paths:
+                d.compress(dp)
 
             return True
         else:
@@ -194,5 +193,8 @@ if __name__ == "__main__":
         atrribute_list = config.ATTRIBUTE_LIST_DEBUG
     else:
         atrribute_list = config.ATTRIBUTE_LIST
-    data = RawData(config.DEBUG_DIR, config.PACS_DIR,
-                   d.joinPath([config.DEBUG_DIR, config.GLOBAL_SUMMARY_FILE]), atrribute_list)
+    data = RawData(config.DEBUG_DIR,
+                   config.PACS_DIR,
+                   d.joinPath([config.DEBUG_DIR, config.GLOBAL_SUMMARY_FILE]),
+                   atrribute_list
+                   )
