089b268a4e962c3d213d1e21718ad5d43f59a9b3
Piotr Faba <pdfaba@gmail.com>
2021-01-23 13:02:26 +0100

    added arguement parser

--- bin/filter_copy.py
+++ bin/filter_copy.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
+
 import datetime as dt
 import os, sys
 sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import directory
+import argparse
 
 def main():
     today = dt.date.today()
     date_pacs_format = today.strftime("%Y%m%d")
-    print(date_pacs_format)
-    # dataPaths = directory.getChildrenPaths(dateFolderPath)
+    parser = argparse.ArgumentParser(description='This program copies selected DICOM files from 1 directory to another',
+                                     prog='DICOM filter copy at MCB, UJ',
+                                     usage='python filter_copy.py -input /some/path -output /other/path ',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-input', help='Path to folder with folders and files', required=True)
+    parser.add_argument('-output', help='Output path', required=True)
+    parser.add_argument('-date', help='Search files created on date (YYMMDD); Default is current date', default=date_pacs_format)
+
+    args = parser.parse_args()
+    # print(args.date)
+    # dataPaths = directory.getChildrenPaths(date_pacs_format)
 
 if __name__ == "__main__":
     main()
